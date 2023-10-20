6a84bad51d1408119678eddc1eb919e3e24768d7
Kristofi Misi <krmihaly42@gmail.com>
2023-10-20 15:58:11 +0200

    remove configured flag and exceptions from control loop

--- src/control_node.cpp
+++ src/control_node.cpp
@@ -31,15 +31,7 @@ int main(int argc, char ** argv)
   auto qos = rclcpp::QoS(rclcpp::KeepLast(1));
   qos.best_effort();
 
-  std::atomic_bool is_configured = false;
-  auto is_configured_sub = controller_manager->create_subscription<std_msgs::msg::Bool>(
-    "robot_manager/is_configured", qos,
-    [&is_configured](std_msgs::msg::Bool::SharedPtr msg) {
-      is_configured = msg->data;
-    });
-
-
-  std::thread control_loop([controller_manager, &is_configured]() {
+  std::thread control_loop([controller_manager]() {
       struct sched_param param;
       param.sched_priority = 95;
       if (sched_setscheduler(0, SCHED_FIFO, &param) == -1) {
@@ -50,25 +42,13 @@ int main(int argc, char ** argv)
           "You can use the driver but scheduler priority was not set");
       }
 
-      const rclcpp::Duration dt =
-      rclcpp::Duration::from_seconds(1.0 / controller_manager->get_update_rate());
-      std::chrono::milliseconds dt_ms {1000 / controller_manager->get_update_rate()};
+      while (rclcpp::ok()) {
+        const rclcpp::Duration dt =
+        rclcpp::Duration::from_seconds(1.0 / controller_manager->get_update_rate());
 
-      try {
-        while (rclcpp::ok()) {
-          if (is_configured) {
-            controller_manager->read(controller_manager->now(), dt);
-            controller_manager->update(controller_manager->now(), dt);
-            controller_manager->write(controller_manager->now(), dt);
-          } else {
-            controller_manager->update(controller_manager->now(), dt);
-            std::this_thread::sleep_for(dt_ms);
-          }
-        }
-      } catch (std::exception & e) {
-        RCLCPP_ERROR(
-          controller_manager->get_logger(), "Quitting control loop due to: %s",
-          e.what());
+        controller_manager->read(controller_manager->now(), dt);
+        controller_manager->update(controller_manager->now(), dt);
+        controller_manager->write(controller_manager->now(), dt);
       }
     });
 
